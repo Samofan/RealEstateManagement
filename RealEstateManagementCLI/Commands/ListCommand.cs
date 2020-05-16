f2c59dfd6e38e97c0a3fd5fdb2fb5526379bdace
Samofan <dinter.florian@googlemail.com>
2020-05-16 17:05:52 +0200

    Add list command to CLI

--- RealEstateManagementCLI/Commands/ListCommand.cs
+++ RealEstateManagementCLI/Commands/ListCommand.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
+using CliFx.Exceptions;
 
-namespace RealEstateManagementCLI
+namespace RealEstateManagementCLI.Commands
 {
-    [Command("list")]
+    [Command("list", Description = "List available real estates.")]
     public class ListCommand : ICommand
     {
         [CommandOption("alphabetical", 'a', Description = "Sort by alphabetic order.")]
@@ -12,11 +14,24 @@ namespace RealEstateManagementCLI
 
         [CommandOption("housesOnly", 'o', Description = "Show houses only.")]
         public bool ShowHousesOnly { get; set; } = false;
+
+        [CommandOption("apartmentsOnly", 'p', Description = "Show apartments only.")]
+        public bool ShowApartmentsOnly { get; set; } = false;
         
         public ValueTask ExecuteAsync(IConsole console)
         {
+            if (ShowApartmentsOnly && ShowHousesOnly)
+            {
+                throw new CommandException("You have to decide!");
+            }
+            else
+            {
             console.Output.WriteLine("Sort by alphabet: " + SortAlphabetical);
             console.Output.WriteLine("Show houses only: " + ShowHousesOnly);
+            console.Output.Write("Show apartments only: " + ShowApartmentsOnly);    
+            }
+
+            
 
             return default;
         }

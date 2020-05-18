46100daa3f4efd3b780adc7953093ccb68148449
Samofan <dinter.florian@googlemail.com>
2020-05-18 14:57:31 +0200

    Add serialization to hard drive

---
+++ RealEstateManagementLibrary/Utils/Serialization/ISavePOCOS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateManagementLibrary.Utils.Serialization
+{
+    public interface ISavePOCOS
+    {
+        void SaveAsJson();
+
+        void SaveInDatabase();
+
+        void SaveAsXml();
+    }
+}
---
+++ RealEstateManagementLibrary/Utils/Serialization/SavePOCOSImpl.cs
@@ -0,0 +1,55 @@
+﻿using RealEstateManagementLibrary.Models;
+using RealEstateManagementLibrary.Models.RealEstate;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace RealEstateManagementLibrary.Utils.Serialization
+{
+    class SavePOCOSImpl : ISavePOCOS
+    {
+        string filePath;
+
+        public SavePOCOSImpl(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void SaveAsJson()
+        {
+            RealEstate realEstate = new House
+            {
+                ForSale = true,
+                PurchasePrice = 100000,
+                Address = new Address
+                {
+                    Country = "Germany",
+                    City = "Siegen",
+                    State = "NRW",
+                    ZipCode = "57072",
+                    Street = "Frankfurter Straße",
+                    HouseNumber = "32"
+                },
+                AmountOfRooms = 5,
+                PlotSize = 250
+            };
+
+            string jsonString;
+            jsonString = JsonSerializer.Serialize(realEstate);
+
+            File.WriteAllText(filePath + "/RealEstate.json", jsonString);
+        }
+
+        public void SaveAsXml()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveInDatabase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

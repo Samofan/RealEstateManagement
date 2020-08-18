1638239d2927d9874fd7f9f141b3935593db478a
Samofan <dinter.florian@googlemail.com>
2020-08-18 11:02:08 +0200

    Add possibility to change the file extension after changing serialization

--- RealEstateManagementCLI/Commands/SerializationCommand.cs
+++ RealEstateManagementCLI/Commands/SerializationCommand.cs
@@ -16,9 +16,13 @@ namespace RealEstateManagementCLI.Commands
 
         [CommandOption("show", 's', Description = "Show the specified serialization type.")]
         public bool ShowType { get; set; } = false;
+
+        private IConsole _console;
         
         public ValueTask ExecuteAsync(IConsole console)
         {
+            _console = console;
+            
             if (ShowType)
             {
                 console.Output.WriteLine(AppConfiguration.ReadSerializationType().ToString());               
@@ -29,8 +33,13 @@ namespace RealEstateManagementCLI.Commands
             if (Type == SerializationType.Binary.ToString() || Type == SerializationType.Xml.ToString())
             {
                 AppConfiguration.SpecifySerializationType(Enum.Parse<SerializationType>(Type));
-                // TODO: Change file extension by changing serialization type.
-                console.Output.WriteLine("Serialization type changed. Please make sure to change the file extension!");
+                
+                console.Output.WriteLine("Serialization type changed!");
+
+                if (ConfirmExtensionChange(Enum.Parse<SerializationType>(Type)))
+                {
+                    ChangeFileExtension(Enum.Parse<SerializationType>(Type));
+                }
             }
             else
             {
@@ -39,6 +48,57 @@ namespace RealEstateManagementCLI.Commands
             }
 
             return default;
+        }
+
+        /// <summary>
+        /// Asks the user if she/he wants to update the file extension of the existing file.
+        /// </summary>
+        /// <param name="serializationType">The new serialization type</param>
+        /// <returns>True if the user wants to update the serialization type</returns>
+        private bool ConfirmExtensionChange(SerializationType serializationType)
+        {
+            _console.Output.Write("Do you want to update the file extension of the existing to {0}? (y/n)", serializationType);
+
+            var result = _console.Input.ReadLine();
+
+            switch (result)
+            {
+                case "y":
+                    return true;
+                case "n":
+                    return false;
+                default:
+                    _console.Output.WriteLine("Not possible!");
+                    ConfirmExtensionChange(serializationType);
+                    break;
+            }
+            
+            // TODO: Convert data to have it still available after changing the serialization type.
+
+            return false;
+        }
+        
+        /// <summary>
+        /// Changes the file extension of the existing file
+        /// </summary>
+        /// <param name="serializationType">The new serialization type</param>
+        private void ChangeFileExtension(SerializationType serializationType)
+        {
+            var actualPath = AppConfiguration.ReadFilePath();
+            var extension = System.IO.Path.GetExtension(actualPath);
+
+            // Remove the file extension.
+            var pathWithoutExtension = actualPath.Substring(0, actualPath.Length - extension.Length);
+
+            switch (serializationType)
+            {
+                case SerializationType.Binary:
+                    AppConfiguration.SpecifyPath(pathWithoutExtension + ".dat");
+                    break;
+                case SerializationType.Xml:
+                    AppConfiguration.SpecifyPath(pathWithoutExtension + ".xml");
+                    break;
+            }
         }
     }
 }
